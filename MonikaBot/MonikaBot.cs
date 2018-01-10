6d68adc6a855b568d5a1cbdff7ef84e157322912
Mike Santiago <miketheripper1@gmail.com>
2018-01-09 20:22:46 -0500

    oof

---
+++ MonikaBot/MonikaBot.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus;
+using System;
+
+namespace MonikaBot
+{
+    class MonikaBot : IDisposable
+    {
+        private DiscordClient _client;
+
+        public MonikaBot()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //nothing yet, we have nothing to dispose!
+        }
+    }
+}
