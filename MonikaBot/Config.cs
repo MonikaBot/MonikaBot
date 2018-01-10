c7679574c0b662df1d8f08df46d30019a1042b48
Mike Santiago <miketheripper1@gmail.com>
2018-01-09 20:27:13 -0500

    wow look, i'm doing stuff
    Config class.

--- MonikaBot/Config.cs
+++ MonikaBot/Config.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace MonikaBot
 {
-    class Config
+    internal class MonikaBotConfig
     {
+        [JsonProperty("token")]
+        internal string Token = "Bot user Token here...";
+
+        /// <summary>
+        /// The prefix is how you summon the bot, a character or bit of text that's before each message that lets the bot know that this is going to be for it.
+        /// </summary>
+        [JsonProperty("prefix")]
+        internal string Prefix = "--";
+
+        public MonikaBotConfig LoadConfig(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                return JsonConvert.DeserializeObject<MonikaBotConfig>(sr.ReadToEnd());
+            }
+        }
+
+        public void WriteConfig(string path)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                sw.Write(JsonConvert.SerializeObject(this));
+            }
+        }
     }
 }
