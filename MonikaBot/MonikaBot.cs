2662f979e583262bbeae58a10da0e853b7991483
Mike Santiago <miketheripper1@gmail.com>
2018-01-09 20:40:06 -0500

    get the DiscordClient ready for action

--- MonikaBot/MonikaBot.cs
+++ MonikaBot/MonikaBot.cs
@@ -4,28 +4,38 @@ using System.IO;
 
 namespace MonikaBot
 {
-    class MonikaBot : IDisposable
+    public class MonikaBot : IDisposable
     {
         private DiscordClient client;
         internal MonikaBotConfig config;
 
         public MonikaBot()
         {
-            if(File.Exists("config.json")) //check if the configuration file exists
-            {
-                config = new MonikaBotConfig();
-                config = config.LoadConfig("config.json");
-            }
-            else
+            if(!File.Exists("config.json")) //check if the configuration file exists
             {
                 new MonikaBotConfig().WriteConfig("config.json");
                 Console.WriteLine("Please edit the config file!");
+
+                return;
             }
+
+            config = new MonikaBotConfig();
+            config = config.LoadConfig("config.json");
+            DiscordConfiguration dConfig = new DiscordConfiguration
+            {
+                AutoReconnect = true,
+                EnableCompression = true,
+                LogLevel = LogLevel.Debug,
+                Token = config.Token,
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true
+            };
+            client = new DiscordClient(dConfig);
         }
 
         public void Dispose()
         {
-            //nothing yet, we have nothing to dispose! 
+            //nothing yet, we have nothing to dispose!
         }
     }
 }
